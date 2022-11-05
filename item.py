0181363226a4b1cfd27b27fe868f459dc7a985a5
Huywin500 <102273579+Huywin500@users.noreply.github.com>
2022-11-04 21:05:16 -0500

    item.py
    update comments

--- item.py
+++ item.py
@@ -1,3 +1,4 @@
+#Class to hold information on the specific items in the vending machine
 class Item:
     def __init__(self, price, id, name, quantity):
 	self.price = price
@@ -5,28 +6,28 @@ class Item:
 	self.name = name
 	self.quantity = quantity
     
-    #getter and setter function for price
+    	#getter and setter function for price
     def setPrice(self, p):
         self.price = p
 		
     def getPrice(self):
         return self.price	
 		
-	  #getter and setter function for id
+	#getter and setter function for id
     def setID(self, i):
         self.id = i
 		
     def getID(self):
         return self.id	
 		
-	  #getter and setter function for name
+	#getter and setter function for name
     def setName(self, n):
         self.name = n
 		
     def getName(self):
         return self.name
 	
-	  #getter and setter function for quantity
+	#getter and setter function for quantity
     def setQuantity(self, q):
         self.quantity = q
 		
