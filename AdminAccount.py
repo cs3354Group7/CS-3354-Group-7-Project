167462be75de1666e1d050db26afb599d22ed165
Solomon Wakhungu <65043605+1solomonwakhungu@users.noreply.github.com>
2022-11-06 21:27:17 -0600

    Add description

--- AdminAccount.py
+++ AdminAccount.py
@@ -5,6 +5,15 @@ Created on Wed Nov  2 15:07:31 2022
 @author: kso170000
 """
 
+'''
+The admin account class stores all the information about the admin.
+It stores the username, password, password expiry date, and failed attempts.
+The information about the admin account is able to be updated using the
+updateAccount function. Though, if not all the information is being changed, 
+there is the updatePassword and updateUsername functions to only change the
+password of the username of the admin account. 
+'''
+
 class AdminAccount:
     def __init__(self, userName = None, password = None, passWordExpiryDate = None, failedAttempts = None):
         self.userName = userName
