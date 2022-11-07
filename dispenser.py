a6f81a933ca0f61d8547d9321d0449482d774d84
Solomon Wakhungu <65043605+1solomonwakhungu@users.noreply.github.com>
2022-11-06 21:23:41 -0600

    Update dispenser.py

--- dispenser.py
+++ dispenser.py
@@ -1,12 +1,25 @@
+'''
+The Dispenser class is responsible for checking 
+dispensing items to the user. It stores which items the user
+wants to dispense and the quantity of each item. The dispenseItems
+function returns all the items currently in the dispenser.
+The addItems function adds new items to the items that already 
+exist in the dispenser. 
+'''
+
 class Dispenser:
     def __init__(self, products = {}):
         self.product = products # Stores a product dictionary where the key is the product name 
                                 # and the value if the product quantity.
         self.hasProduct = bool(self.product)
     
+    # Returns the items in the dispenser and resets
+    # the amount of products in the dispenser
     def dispenseItems(self):
         if self.hasProduct:
-            return self.product
+            products = self.product
+            self.product = {}
+            return products
         return {}
     
     def addItems(self, item = {}):
