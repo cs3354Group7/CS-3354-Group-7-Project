7f73ccafd39ead2571564fda2236731bb563ce97
Solomon Wakhungu <1solomonwakhungu@gmail.com>
2022-11-09 18:29:59 -0600

    Add elements in the main page

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask import render_template
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='html')
 
 # Return Home Page
 @app.route("/")
 def home():
-    return "Home Page"
+    return render_template('index.html')
 
 # Return Purchase Page
 @app.route("/purchase")
@@ -26,3 +28,11 @@ def login():
 @app.route("/sign-up")
 def signUp():
     return "Sign Up Page"
+
+# Return Contact Us Page
+@app.route("/contact")
+def contact():
+    return "Contact Page"
+
+if __name__ == "__main__":
+    app.run(debug=True)
